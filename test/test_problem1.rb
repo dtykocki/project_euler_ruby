8ee98236e98281276de6aeb167acbdae421af679
Doug Tykocki <dtykocki@omegafi.com>
2011-06-21 04:49:14 -0400

    Spacing fixes

--- test/test_problem1.rb
+++ test/test_problem1.rb
@@ -5,21 +5,16 @@ require_relative '../lib/euler/problem1'
 
 class Problem1Test < Test::Unit::TestCase
 
-   should "return some stuff" do
+   should "return the sum of all the multiples of 3 or 5 below 10" do
       prb = Euler::Problem1.new
-      assert_equal true, true
+      prb.sum_of_multiples_of_3_or_5_below 10
+      assert_equal 23, prb.sum
    end
 
-	should "return the sum of all the multiples of 3 or 5 below 10" do
-		prb = Euler::Problem1.new
-		prb.sum_of_multiples_of_3_or_5_below 10
-		assert_equal 23, prb.sum
-	end
-
-	should "return the sum of all the multiples of 3 or 5 below 1000" do
-		prb = Euler::Problem1.new
-		prb.sum_of_multiples_of_3_or_5_below 1000
-		assert_equal 233168, prb.sum
-	end
+   should "return the sum of all the multiples of 3 or 5 below 1000" do
+      prb = Euler::Problem1.new
+      prb.sum_of_multiples_of_3_or_5_below 1000
+      assert_equal 233168, prb.sum
+   end
 
 end
